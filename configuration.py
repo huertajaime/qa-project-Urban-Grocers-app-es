05d45cd8cdc414423cbf9f3302d75197a9a99700
jaime <huerta.jaime@gmail.com>
2025-07-22 16:56:09 -0700

    configuration.py update

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-504a0397-8bdf-4f98-8863-9eee0bbd2711.containerhub.tripleten-services.com"
 CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = # Almacena la ruta para crear un kit en esta variable
