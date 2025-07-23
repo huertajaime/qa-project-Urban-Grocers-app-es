6af4ef99a171543a0af40ab58a7bb7f26762999b
jaime <huerta.jaime@gmail.com>
2025-07-22 21:08:18 -0700

    actualizacion de configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-3a7ec54e-8f15-4782-80e1-6d7fe051246d.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-6fb61eb1-98e8-4c92-bf86-beee5a772676.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
